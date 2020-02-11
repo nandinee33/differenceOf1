cb08d71f5a21cd69816791ff32bf652afac8940c
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-11 16:23:01 +0530

    check the index value is less than k. check all other conditions.

--- src/differenceOf1.java
+++ src/differenceOf1.java
@@ -5,7 +5,7 @@ public class differenceOf1 {
 
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        int temp;
+        int temp, n1, n2, n11;
 
         for (int i = 0; i < t; i++) {
             int n = sc.nextInt();
@@ -15,8 +15,22 @@ public class differenceOf1 {
 
             for (int j = 0; j < n; j++) {
                 arr[j] = sc.nextLong();
+                if (arr[j] < k) {
+                    String s = String.valueOf(arr[j]);
+                    if (s.length() >= 2) {
+                        n11 = 0;
+                        for (int k1 = 0; k1 < s.length() - 1; k1++) {
+                            n1 = s.charAt(k1);
+                            n2 = s.charAt(k1 + 1);
+                            if (n1 - n2 == 1 || n2 - n1 == 1) {
+                                n11 = n11 + 1;
+                            } else
+                                break;
+                        }
 
 
+                    }
+                }
             }
         }
     }
