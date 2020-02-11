071e90b15070f7dc3a05304dc6ff9c6750e4ca52
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-11 16:17:57 +0530

    initialising an array of n size and take values from the user

--- src/differenceOf1.java
+++ src/differenceOf1.java
@@ -5,10 +5,19 @@ public class differenceOf1 {
 
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        int temp;
+
         for (int i = 0; i < t; i++) {
             int n = sc.nextInt();
             long k = sc.nextLong();
+            long arr[] = new long[n];
+            temp = 0;
 
+            for (int j = 0; j < n; j++) {
+                arr[j] = sc.nextLong();
+
+
+            }
         }
     }
 }
